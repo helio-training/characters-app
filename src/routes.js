0dd74d74939ca9795b4814116d5697fc1d7cbeda
Tyler Garlick <tjgarlick@gmail.com>
2017-03-02 11:51:15 -0700

    Added pages

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+
 import App from './App';
 import HomePage from './containers/common/HomePage';
+import NewCharacterPage from './containers/common/characters/NewCharacterPage';
+import CharactersPage from './containers/common/characters/CharactersPage';
+import NotFoundPage from './containers/common/NotFoundPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
+    <Route path="characters" component={CharactersPage}/>
+    <Route path="characters/new" component={NewCharacterPage}/>
+    <Route path="*" component={NotFoundPage} />
   </Route>
 )
